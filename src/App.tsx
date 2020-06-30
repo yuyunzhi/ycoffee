c9b788dc1e39089750c9fa57c6e4b9b98652d5b1
yuyunzhi <lambert1129@foxmail.com>
2020-06-30 20:18:21 +0800

    feat: 🎸 添加按钮组件及功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button , { ButtonSize,ButtonType} from './components/Button/button'
 
 function App() {
   return (
@@ -7,6 +8,10 @@ function App() {
         <code>
             const a = b
         </code>
+        <Button> 你好</Button>
+        <Button disabled > 你好</Button>
+        <Button btnType={'primary'} size={'lg'}> 你好</Button>
+        <Button btnType='link' href='https://www.baidu.com' disabled> 你好</Button>
     </div>
   );
 }
---
+++ src/components/Button/button.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import classNames from 'classnames'
+export type ButtonSize = 'lg' | 'sm'
+export type ButtonType = 'primary' | 'default' | 'danger' | 'link'
+
+interface BaseButtonProps {
+    className?: string;
+    /**设置 Button 的禁用 */
+    disabled?: boolean;
+    /**设置 Button 的尺寸 */
+    size?: ButtonSize;
+    /**设置 Button 的类型 */
+    btnType?: ButtonType;
+    children: React.ReactNode;
+    href?: string;
+}
+
+const Button:React.FC<BaseButtonProps> = (props) =>{
+
+    const {
+        btnType,
+        disabled,
+        className,
+        size,
+        children,
+        href,
+        ...restProps
+    } = props
+
+    // btn btn-lg btn-primary
+    const classes = classNames('btn', className, {
+        [`btn-${btnType}`]: btnType,
+        [`btn-${size}`]: size,
+        'disabled': (btnType === 'link') && disabled
+    })
+
+    if(btnType === 'link' && href){
+        return (
+            <a
+                className={classes}
+                href={href}
+                {...restProps}
+            >
+                {children}
+            </a>
+        )
+    }else{
+        return(
+            <button
+                className={classes}
+                disabled={disabled}
+                {...restProps}
+            >
+                {children}
+            </button>
+        )
+    }
+}
+
+Button.defaultProps = {
+    disabled: false,
+    btnType: 'default'
+}
+
+export default Button;
