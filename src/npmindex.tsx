5cd84bec0ad51c37113c3f9dcdd75850ca42149b
yuyunzhi <lambert1129@foxmail.com>
2020-07-02 15:18:33 +0800

    ci: 🎡 change ci config

---
+++ src/npmindex.tsx
@@ -0,0 +1,8 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
+
+export { default as Button } from './components/Button'
+export { default as Icon } from './components/Icon'
+export { default as Input } from './components/Input'
+
