7e246f3950c4188ee6e3225e8bc6e4342bbf2205
yuyunzhi <lambert1129@foxmail.com>
2020-08-31 14:11:36 +0800

    feat:格式化

--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -10,7 +10,6 @@ import React, {
 
 import classNames from 'classnames'
 import Input, { InputProps } from '../Input/input'
-import Icon from '../Icon/icon'
 import Transition from '../Transition/transition'
 import useDebounce from '../../hooks/useDebounce'
 import useClickOutside from '../../hooks/useClickOutside'
