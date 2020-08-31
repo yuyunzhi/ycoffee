f1ababde5dfe9755ab58176902dd97191b7fd56f
yuyunzhi <lambert1129@foxmail.com>
2020-08-31 16:17:29 +0800

    feat:build storybook

---
+++ storybook-static/precache-manifest.6462998698c652a20f84751cb2eef513.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c512158beebfa6fa688bb90cc9ee156",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.83c7f80a045e82bdab33.bundle.js"
+  },
+  {
+    "url": "runtime~main.83c7f80a045e82bdab33.bundle.js"
+  },
+  {
+    "revision": "7695376ee58f225ad331",
+    "url": "static/css/main.ddc88b72.chunk.css"
+  },
+  {
+    "url": "vendors~main.83c7f80a045e82bdab33.bundle.js"
+  },
+  {
+    "url": "vendors~main.83c7f80a045e82bdab33.bundle.js.LICENSE.txt"
+  }
+]);
