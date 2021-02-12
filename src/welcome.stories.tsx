ec2da3d387a7ee0490242af9477a7f26f3af5f09
yuyunzhi <lambert1129@foxmail.com>
2021-02-12 17:42:14 +0800

    docs: ✏️ update first page

--- src/welcome.stories.tsx
+++ src/welcome.stories.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import "./styles/index.scss";
+import "./styles/storybook.scss";
 
-storiesOf("Welcome page", module).add(
-  "welcome",
-  () => {
-    return (
-      <>
-        <h1>欢迎来到 ycoffee 组件库</h1>
-        <p>
-          {" "}
-          ycoffee 是 一个好用的React组件库，技术栈使用了React Hooks + TypeScript
-        </p>
-        <h3>安装试试</h3>
-        <code>npm install ycoffee --save</code>
-      </>
-    );
-  },
-  { info: { disable: true } }
+const defaultWelcome = () => (
+  <div className="story-container">
+    <div className="story-header">欢迎来到 ycoffee 组件库</div>
+    <div className="story-demo-box">
+      <div className="story-demo-main">
+        <div className="story-content">
+          <p>
+            {" "}
+            ycoffee 是 一个好用的React组件库，技术栈使用了React Hooks +
+            TypeScript
+          </p>
+          <h3>安装试试</h3>
+          <code>npm install ycoffee --save</code>
+        </div>
+      </div>
+    </div>
+  </div>
 );
+
+storiesOf("Welcome page", module).add("welcome", defaultWelcome, {
+  info: { disable: true },
+});
