833eb410259bebd93e738ea903f0876fcd30fd7b
yuyunzhi <lambert1129@foxmail.com>
2021-02-19 09:51:16 +0800

    feat: delete files about example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Button from "./components/Button/index";
-import X from "./components/Layout/layout.example";
 function App() {
   return (
     <div className="App">
       <Button>按钮</Button>
-      <X />
     </div>
   );
 }
