cbb2b93a9747485ef4efe9d8986b481afbc48959
yuyunzhi <lambert1129@foxmail.com>
2021-02-25 09:53:37 +0800

    feat: npm publish

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles/index.scss";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fas);
 
-ReactDOM.render(
-  // <React.StrictMode>
-  <App />,
-  // </React.StrictMode>,
-  document.getElementById("root")
-);
+export { default as Button } from "./components/Button";
+export { default as Menu } from "./components/Menu";
+export { default as AutoComplete } from "./components/AutoComplete";
+export { default as Icon } from "./components/Icon";
+export { default as Input } from "./components/Input";
+export { default as Progress } from "./components/Progress";
+export { default as Transition } from "./components/Transition";
+export { default as Upload } from "./components/Upload";
+export { default as Dialog } from "./components/Dialog";
+export { confirm, alert, modal } from "./components/Dialog";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export { default as Layout } from "./components/Layout";
+export { default as Header } from "./components/Layout";
+export { default as Footer } from "./components/Layout";
+export { default as Content } from "./components/Layout";
+export { default as Aside } from "./components/Layout";
+
+export { default as CitySelect } from "./components/CitySelect";
+export { default as Scroll } from "./components/Scroll";
+export { default as Tree } from "./components/Tree";
+export { default as Checkbox } from "./components/Checkbox";
+export { default as Breadcrumb } from "./components/Breadcrumb";
