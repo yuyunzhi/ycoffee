0f8a5196090ee607d832ffef2b09fccfbe2098c6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-02-25 01:55:40 +0000

    Deploy yuyunzhi/ycoffee to github.com/yuyunzhi/ycoffee.git:gh-pages

---
+++ main.401e9994f7d283f6a693.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1253:function(n,o,c){"use strict";c.r(o);c(1254),c(1286)},452:function(n,o,c){c(431),c(1253),n.exports=c(578)},495:function(n,o){}},[[452,1,2]]]);
---
+++ precache-manifest.bbe00ec29831b22515610a9cd5feb616.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "346b6f9cd1e01b03451de25d285c93cd",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.69b5e27951987f942e11.bundle.js"
+  },
+  {
+    "url": "runtime~main.69b5e27951987f942e11.bundle.js"
+  },
+  {
+    "revision": "00bf7362b4122e459de4",
+    "url": "static/css/main.a17a2148.chunk.css"
+  },
+  {
+    "revision": "2d0a0021707c71b94090",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "revision": "c03fca72d6898721d041642a7f494d52",
+    "url": "static/media/down.c03fca72.png"
+  },
+  {
+    "url": "vendors~main.69b5e27951987f942e11.bundle.js"
+  },
+  {
+    "url": "vendors~main.69b5e27951987f942e11.bundle.js.LICENSE.txt"
+  }
+]);
